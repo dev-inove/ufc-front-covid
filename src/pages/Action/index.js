2b930d0573e382d2b0149bf670b51c601b91b92f
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2020-10-15 22:56:37 -0300

    Fixing bugs on scss and starting the structure os the pages

--- src/pages/Action/index.js
+++ src/pages/Action/index.js
@@ -1,16 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "../../services/api-back";
 import { Link } from "react-router-dom";
+import Header from "../../components/Header/Navbar";
+import "./style.scss";
+
 export default function Action() {
+  useEffect(() => {
+    // async function load() {
+    //   const { data } = await api.post("/sessions", {
+    //     email: "bianor.alvaro12@gmail.com",
+    //     password: "Alvim",
+    //   });
+    //   console.log(data);
+    // }
+    // load();
+  }, []);
+
+  const [email, setEmail] = useState("");
+  const [action, setAction] = useState([]);
+
+  async function submitt(e) {
+    e.preventDefault();
+
+    const { data } = await api.get(`/action?title=${email}`);
+
+    setAction(data.action);
+  }
+
   return (
     <>
-      <h1>Alvaro Bianor</h1>
-      <h2>Sousa Medeiros</h2>
+      <Header />
+      <div className="searchBar">
+        <button>
+          <Link to="/" className="nothing">
+            Vaaaai
+          </Link>
+        </button>
+        <br />
 
-      <button>
-        <Link to="/" className="nothing">
-          Vaaaai
-        </Link>
-      </button>
+        <form onSubmit={submitt}>
+          <input
+            type="text"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <button type="submit">Go</button>
+        </form>
+      </div>
+
+      <div className="alvaro">
+        {action.map((data, i) => {
+          return <p key={i}>{data.title}</p>;
+        })}
+      </div>
     </>
   );
 }
