81e9079ca13e17345fa1d1bf26675c53bab475e5
Alvaro Bianor Sousa Medeiros <bianor.alvaro12@gmail.com>
2021-02-25 11:25:19 -0300

    Adding api to get infos from covid

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -1,28 +1,55 @@
-import React from 'react';
-import Box from './Box'
+import React, { useEffect, useState } from "react";
+import Box from "./Box";
 import "./styles/banner.scss";
-const Banner = () => (
-<>
-<header className="header" id="home">
+import api from "../../services/api";
+
+const Banner = () => {
+  const [data, setData] = useState({});
+
+  const getData = async () => {
+    try {
+      const {
+        data: { data },
+      } = await api.get("");
+      setData(data.filter((e) => e.uf === "CE")[0]);
+    } catch (e) {
+      setData({
+        cases: 415664,
+        deaths: 11087,
+        refuses: 89,
+        state: "Ceará",
+        suspects: 493,
+        uf: "CE",
+      });
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(data);
+  return (
+    <>
+      <header className="header" id="home">
         <div className="header__title pad-sm">
-            <h1>
-                UFC Contra
-                <br />o Coronavírus
-            </h1>
-            <h2>
-                Confira os principais destaques quanto as ações promovidas
-                pela UFC em combate ao Coronavírus no estado do Ceará.
-            </h2>
+          <h1>
+            UFC Contra
+            <br />o Coronavírus
+          </h1>
+          <h2>
+            Confira os principais destaques quanto as ações promovidas pela UFC
+            em combate ao Coronavírus no estado do Ceará.
+          </h2>
         </div>
-        
-        <div  id="actions"></div>
+
+        <div id="actions"></div>
 
         <div className="header__info">
-            <Box title="Confirmados" num="259764"  />
-            <Box title="Recuperados" num="89" />
-            <Box title="Mortes" num="9130" />
+          <Box title="Confirmados" num={data.cases} />
+          <Box title="Recuperados" num={data.refuses} />
+          <Box title="Mortes" num={data.deaths} />
         </div>
-    </header>
-</>
-);
-export default Banner; 
+      </header>
+    </>
+  );
+};
+export default Banner;
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,5 @@
-import axios from 'axios';
-const api = axios.create({baseURL:'https://covid19-brazil-api.now.sh/api/report/v1/brazil/'});
+import axios from "axios";
+const api = axios.create({
+  baseURL: "https://covid19-brazil-api.now.sh/api/report/v1/",
+});
 export default api;
